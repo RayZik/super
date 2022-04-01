ec44c4efcd4b9f8f6df90e7bfeab8949ebd8385d
alex <mralexrabota@gmail.com>
2022-04-01 04:29:41 +0000

    My init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// This is a basic program on JavaScript
+//
+// Try to modify and run it and check out
+// the output in the terminal below
+//
+// Happy coding! :-)
+
+'use strict';
+
+console.log('Hello, World!');
